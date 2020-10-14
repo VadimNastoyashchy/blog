ead903930b7cb0faa649b4b034b94465b9f80f0a
vadymnastoiashchyi <vadim.89@ukr.net>
2020-10-14 11:43:52 +0300

    Add post method

--- src/main/java/com/example/blog/models/Post.java
+++ src/main/java/com/example/blog/models/Post.java
@@ -14,8 +14,17 @@ public class Post {
 
     private String title;
     private String anons;
-    private String fullText;
+    private String full_text;
     private int views;
+
+    public Post() {
+    }
+
+    public Post(String title, String anons, String full_text) {
+        this.title = title;
+        this.anons = anons;
+        this.full_text = full_text;
+    }
 
     public Long getId() {
         return id;
@@ -41,12 +50,12 @@ public class Post {
         this.anons = anons;
     }
 
-    public String getFullText() {
-        return fullText;
+    public String getFull_text() {
+        return full_text;
     }
 
-    public void setFullText(String fullText) {
-        this.fullText = fullText;
+    public void setFull_text(String fullText) {
+        this.full_text = full_text;
     }
 
     public int getViews() {
